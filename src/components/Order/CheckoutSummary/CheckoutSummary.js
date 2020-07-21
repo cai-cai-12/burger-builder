c1021e728953f7b66d88be4b66bcb8fe73291a93
cai <trieungochai.dev@gmail.com>
2020-07-22 01:47:58 +0700

    commited

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+
+// The goal here is to display a preview of our burger & show the continue or cancel btn
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>Well, we hope it tastes good!</h1>
+            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+
+            </div>
+        </div>
+    );
+}
+
+export default checkoutSummary;
