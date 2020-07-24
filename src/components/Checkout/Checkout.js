56ee19698e58551015fbf68a0813759d1b0ac2a3
cai <trieungochai.dev@gmail.com>
2020-07-24 23:42:43 +0700

    24.07

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,29 +1,32 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     // we'll symply pass a dummy ingredients obj
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1,
-        }
+        ingredients: null,
+        price: 0
     }
 
     // implement the logic to pass the real ingredients we picked on the checkout container
     // won't use componentDidUpdate() or anything like that
     // because whenever we load this component, it'll mount itself.
     // there is no way we can route to it without it being mounted again because it's not nested in some other page or anything like that.
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
 
     checkoutCancelledHandler = () => {
@@ -45,6 +48,13 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler} 
                     checkoutContinued={this.checkoutContinuedHandler} />
+                <Route 
+                    path={this.props.match.path + '/contact-data'} 
+                    // component={ContactData}
+                    // we can output some JSX on the right side of the arrow with the same result
+                    // However since we now render it manually here, we can pass props to it
+                    render={(props) => <ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>}
+                    />
             </div>
         );
     }
--- src/components/Checkout/ContactData/ContactData.js
+++ src/components/Checkout/ContactData/ContactData.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+import axios from '../../../axios-orders';
+
+import Button from '../../UI/Button/Button';
+import classes from './ContactData.css';
+import Spinner from '../../UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -7,13 +12,78 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: '',
+        },
+        loading: false,
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+
+        // we need access to the ingredients & contact data to make this request though
+        // we set up this props in file Checkout.js (line 56), so now we can access the ingredients on props
+    
+        // call this.setState and set loading to true, 
+        // because we're loading the request is about to get sent.
+        // Once the "Response => console.log(Response)" though
+        this.setState({loading: true});
+
+        // use axios instance to send the request to our backend
+        // for storing data, we should use a post request and therefore we use the post() method on that instance
+        // now we also need to send some data and that data should be our order for the given burger config.
+        const order = {
+            ingredients: this.props.ingredients,
+            // the totalPrice is only calculated & stored in the BurgerBuilder
+            // so we have to pass the totalPrice along with the ingredients from the BurgerBuilder to the Checkout component.
+            price: this.props.price,
+            customer: {
+                name: 'Hai Trieu',
+                address: {
+                    street: 'Dinh Tien Hoang',
+                    zipCode: '70000',
+                    country: 'Viet Nam'
+                },
+                email: 'trieungochai.dev@gmail.com'
+            },
+            deliveryMethod: 'fastest'
         }
+        axios.post('/orders.json', order)
+            .then(Response => {
+                // want to stop loading no matter what the response is 
+                // because the request is done even if it failed.
+                
+                // set the loading to false again -> go back to the 'order'
+                this.setState({loading: false});
+                this.props.history.push('/');
+
+            })
+            .catch(error => {
+                // want to set loading to false if we have an err
+                // because even if an err occurred, we want to stop loading
+                // and we don't want to show the spinner anymore 
+                // because our UI would be stuck in this case and the user would think it's still loading
+                // => close the Modal 
+                // because the modal only shown if state.purchasing props = true -> so in both cases, we'll set purchasing = false
+                this.setState({loading: false})
+            });
     }
 
     render() {
+        let form = (
+            <form>
+                <input className={classes.Input} type='text' name='name' placeholder='Your Name' />
+                <input className={classes.Input} type='email' name='email' placeholder='Your Mail' />
+                <input className={classes.Input} type='text' name='street' placeholder='Street' />
+                <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
+                <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return (
-            <div>
-                
+            <div className={classes.ContactData}>
+                <h4>Please enter your contact data!</h4>
+                {form}
             </div>
         );
     }
