57091dcba169587d8b1ec00b6068a6288042a999
cai <trieungochai.dev@gmail.com>
2020-07-26 16:09:11 +0700

    26.07

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    // we can use componentDidMount() here 
+    // because we only want to fetch orders when this is loaded
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(Response => {
+                const fetchedOrders = [];
+                for (let key in Response.data) {
+                    fetchedOrders.push({
+                        ...Response.data[key],
+                        id: key
+                    });
+                }
+                this.setState({loading: false, orders: fetchedOrders})
+            })
+            .catch(error => {
+                this.setState({loading: false})
+            });
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}/>
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
