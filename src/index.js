a48e81bb46c2e45ac3b1d52d2cf579865c7f66c7
cai <trieungochai.dev@gmail.com>
2020-07-30 03:45:37 +0700

    install redux-thunk & Redux DevTools

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 // let's set up the state management in the index.js file where we wrap our entire app (add provider component)
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
+
 // with that - we can set the store property on the Provider component
-const store = createStore(reducer);
+const store = createStoreWithMiddleware(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
